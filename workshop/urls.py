b12823ba00023d87ef2297f185231320f7982fb8
root <root@11CPU0117L.amritavidya.edu>
2016-06-01 16:09:31 +0530

    third commit

--- workshop/urls.py
+++ workshop/urls.py
@@ -1,12 +1,15 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from registration.views import Home
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'workshop.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+   # Examples:
+                      #1. Add an import:  from other_app.views import Home
+ # 2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+   # url(r'^$', 'workshop.views.home', name='home'),
+   # url(r'^blog/', include('blog.urls')),
+   url(r'^admin/', include(admin.site.urls)),
+   url(r'^$', Home.as_view(), name='home')
 )
