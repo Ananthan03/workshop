8094315a116c5d37a62474ab9c8aa87292a477a6
Ananthan03 <ananthakrishnanm03@gmail.com>
2016-06-03 13:04:43 +0530

    9th commit

--- workshop/settings.py
+++ workshop/settings.py
@@ -59,3 +59,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = (MEDIA_ROOT,)
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
+LOGIN_REDIRECT_URL = 'home'
+AUTH_USER_MODEL = "registration.User"
--- workshop/urls.py
+++ workshop/urls.py
@@ -1,27 +1,17 @@
-"""mysite URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.9/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
-from django.conf.urls import url
+from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from registration.views import Home
-from registration import urls as reg_url
-from django.conf.urls import include, url
-urlpatterns = [
-        url(r'^$', Home.as_view(), name='home'),
-        url(r'^admin/', include(admin.site.urls)),
-        url(r'^registration/', include(reg_url)),
-
-]
-
+from registration import urls as reg_urls
+from django.contrib.auth import views as auth_views
+from workshop.views import anonymous_required
+admin.autodiscover()
+urlpatterns = patterns('',
+   # Examples:
+   # url(r'^blog/', include('blog.urls')),    url(r'^admin/', include(admin.site.urls)),
+   url(r'^registration/', include(reg_urls)),
+   url(r'^$', Home.as_view(), name='home'),
+   url(r'^user/login/$',
+       anonymous_required(auth_views.login),
+       {'template_name': 'login.html'},
+       name='login')
+)
