eb3d15637e89addc16a70e474fc6c364e7e990a8
Ananthan03 <ananthakrishnanm03@gmail.com>
2016-06-04 12:54:50 +0530

    logout

--- registration/models.py
+++ registration/models.py
@@ -1,13 +1,9 @@
-from __future__ import unicode_literals
-
 from django.db import models
-
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.utils.translation import ugettext_lazy as _
 from django.core.validators import RegexValidator, MaxValueValidator, MinValueValidator
-
-# from mail_templated import send_mail
+#from mail_templated import send_mail
 
 # Create your models here.
 GENDER = (('M', _('Male')), ('F', _('Female')))
@@ -18,7 +14,7 @@ class MyUserManager(BaseUserManager):
         """
         Creates and saves a User with the given email, email and password.
         """
-        if not email: raise ValueError('Users must have an email address')
+        if not email:raise ValueError('Users must have an email address')
         user = self.model(email=self.normalize_email(email))
         user.set_password(password)
         user.save(using=self._db)
@@ -33,7 +29,6 @@ class MyUserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-
 class User(AbstractBaseUser):
     """
     Extends the default User profiles of Django. The fields of this model can be obtained by the
@@ -41,17 +36,17 @@ class User(AbstractBaseUser):
     """
     user_id = models.AutoField(primary_key=True)
     user_first_name = models.CharField(_('First Name'), max_length=32, blank=True, null=True,
-                                       validators=[RegexValidator(regex='^[A-Za-z]*$')])
+                                  validators=[RegexValidator(regex='^[A-Za-z]*$')])
     user_last_name = models.CharField(_('Last Name'), max_length=32, blank=True, null=True,
-                                      validators=[RegexValidator(regex='^[A-Za-z]*$')])
+                                    validators=[RegexValidator(regex='^[A-Za-z]*$')])
     email = models.EmailField(_('Email'), db_index=True, unique=True)
     user_dob = models.DateField(_('Birth Date'), blank=True, null=True)
     user_gender = models.CharField(_('Gender'), max_length=1, choices=GENDER, blank=True, null=True)
-    user_github = models.CharField(_('Github Profile'), max_length=100, blank=True)
-    user_linkedin = models.CharField(_('Linkedin Profile'), max_length=100, blank=True)
-    user_bio = models.CharField(_('Short Bio'), max_length=1000, blank=True)
-    user_occupation = models.CharField(_('Occupation'), max_length=100, blank=True)
-    user_nationality = models.CharField(_('Nationality'), max_length=100, blank=True)
+    user_github = models.CharField(_('Github Profile'),max_length=100,blank=True )
+    user_linkedin = models.CharField(_('Linkedin Profile'),max_length=100,blank=True )
+    user_bio = models.CharField(_('Short Bio'), max_length=1000,blank=True )
+    user_occupation = models.CharField(_('Occupation'), max_length=100,blank=True )
+    user_nationality = models.CharField(_('Nationality'), max_length=100,blank=True )
     date_joined = models.DateTimeField(auto_now_add=True)
     username = models.CharField(_('username'), max_length=32, blank=False, unique=True)
     is_active = models.BooleanField(default=True)
@@ -62,9 +57,10 @@ class User(AbstractBaseUser):
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
 
+
     def get_name(self):
         if self.user_first_name and self.user_last_name:
-            return (self.user_first_name + ' ' + self.user_last_name)
+            return (self.user_first_name +' '+ self.user_last_name )
         else:
             return self.username
 
@@ -98,8 +94,3 @@ class User(AbstractBaseUser):
 
     def __unicode__(self):
         return self.username
-
-
-from django.db import models
-
-# Create your models here.
